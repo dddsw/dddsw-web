5d0438d817f35db8c31eca097a69a4ccb2da3bf8
Hannah Price <hannahprice94@hotmail.co.uk>
2022-10-22 10:20:45 +0100

    Added hover color invert to footer links

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,9 +1,7 @@
 <div class="footer primary-bg">
-	<a href="https://twitter.com/dddsouthwest"
-		><i class="fa-brands fa-twitter fa-3x" style="color: black;" /></a
-	>
+	<a href="https://twitter.com/dddsouthwest"><i class="fa-brands fa-twitter fa-3x" /></a>
 	<a href="https://www.linkedin.com/company/ddd-south-west/"
-		><i class="fa-brands fa-linkedin fa-3x" style="color: black;" /></a
+		><i class="fa-brands fa-linkedin fa-3x" /></a
 	>
 </div>
 
@@ -13,5 +11,13 @@
 		display: flex;
 		justify-content: flex-end;
 		gap: 30px;
+	}
+
+	.fa-brands {
+		color: black;
+	}
+
+	.fa-brands:hover {
+		color: white;
 	}
 </style>
