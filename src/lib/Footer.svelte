8dfad19bab85dac70f7fd872d358c80d95c3e2f5
Hannah Price <hannahprice94@hotmail.co.uk>
2022-10-23 13:28:05 +0100

    Added first pw tests

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,8 @@
 <div class="footer primary-bg">
-	<a href="https://twitter.com/dddsouthwest"><i class="fa-brands fa-twitter fa-3x" /></a>
-	<a href="https://www.linkedin.com/company/ddd-south-west/"
+	<a href="https://twitter.com/dddsouthwest" target="_blank"
+		><i class="fa-brands fa-twitter fa-3x" /></a
+	>
+	<a href="https://www.linkedin.com/company/ddd-south-west/" target="_blank"
 		><i class="fa-brands fa-linkedin fa-3x" /></a
 	>
 </div>
