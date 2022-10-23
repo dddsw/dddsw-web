f8fcca8900b5f13b375d30242b5c37dd22c5d75a
Hannah Price <hannahprice94@hotmail.co.uk>
2022-10-23 13:30:38 +0100

    ran svelte check and lint

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test('Can navigate to About from Home by clicking nav link', async ({ page }) => {
-  // Go to Home
-  await page.goto('http://127.0.0.1:5173/');
+	// Go to Home
+	await page.goto('http://127.0.0.1:5173/');
 
-  // Click About link
-  await page.locator('.nav-button a[href="/about"]').click();
-  await expect(page).toHaveURL('http://127.0.0.1:5173/about');
+	// Click About link
+	await page.locator('.nav-button a[href="/about"]').click();
+	await expect(page).toHaveURL('http://127.0.0.1:5173/about');
 });
 
 test('Can navigate to Home from About by clicking logo', async ({ page }) => {
-  // Go to About
-  await page.goto('http://127.0.0.1:5173/about');
+	// Go to About
+	await page.goto('http://127.0.0.1:5173/about');
 
-  // Click logo
-  await page.locator('img[class~="logo"]').click();
-  await expect(page).toHaveURL('http://127.0.0.1:5173/');
+	// Click logo
+	await page.locator('img[class~="logo"]').click();
+	await expect(page).toHaveURL('http://127.0.0.1:5173/');
 });
