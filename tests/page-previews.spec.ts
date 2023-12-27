7e6b566d175130c8912e8ee5d0a761fb96cfb90e
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-12-27 20:31:19 +0000

    Update page preview images
    Also ensure that maps are loaded before taking the screenshot

--- tests/page-previews.spec.ts
+++ tests/page-previews.spec.ts
@@ -1,31 +1,22 @@
 // Tests to generate page previews in different screen sizes
-import { test, devices } from '@playwright/test';
+import { test, devices, type Page } from '@playwright/test';
 
 test('Captures Home preview - mobile', async ({ page }) => {
 	await page.setViewportSize(devices['iPhone SE'].viewport);
 
-	// Go to Home page
-	await page.goto('/');
-
-	await page.screenshot({ path: 'tests/page-previews/home-mobile.png', fullPage: true });
+	await savePagePreviewImage(page, '/', 'home-mobile');
 });
 
 test('Captures Home preview - tablet', async ({ page }) => {
 	await page.setViewportSize(devices['iPad Mini'].viewport);
 
-	// Go to Home page
-	await page.goto('/');
-
-	await page.screenshot({ path: 'tests/page-previews/home-tablet.png', fullPage: true });
+	await savePagePreviewImage(page, '/', 'home-tablet');
 });
 
 test('Captures Home preview - desktop', async ({ page }) => {
 	await page.setViewportSize(devices['Desktop Chrome'].viewport);
 
-	// Go to Home page
-	await page.goto('/');
-
-	await page.screenshot({ path: 'tests/page-previews/home-desktop.png', fullPage: true });
+	await savePagePreviewImage(page, '/', 'home-desktop');
 });
 
 // Content pages
@@ -34,27 +25,29 @@ for (const route of routes) {
 	test(`Captures ${route} preview - mobile`, async ({ page }) => {
 		await page.setViewportSize(devices['iPhone SE'].viewport);
 
-		// Go to page
-		await page.goto(`http://127.0.0.1:5173/${route}`);
-
-		await page.screenshot({ path: `tests/page-previews/${route}-mobile.png`, fullPage: true });
+		await savePagePreviewImage(page, route, `${route}-mobile`);
 	});
 
 	test(`Captures ${route} preview - tablet`, async ({ page }) => {
 		await page.setViewportSize(devices['iPad Mini'].viewport);
 
-		// Go to page
-		await page.goto(`http://127.0.0.1:5173/${route}`);
-
-		await page.screenshot({ path: `tests/page-previews/${route}-tablet.png`, fullPage: true });
+		await savePagePreviewImage(page, route, `${route}-tablet`);
 	});
 
 	test(`Captures ${route} preview - desktop`, async ({ page }) => {
 		await page.setViewportSize(devices['Desktop Chrome'].viewport);
 
-		// Go to page
-		await page.goto(`http://127.0.0.1:5173/${route}`);
-
-		await page.screenshot({ path: `tests/page-previews/${route}-desktop.png`, fullPage: true });
+		await savePagePreviewImage(page, route, `${route}-desktop`);
 	});
+}
+
+async function savePagePreviewImage(page: Page, route: string, imageName: string) {
+	// Go to page
+	await page.goto(route);
+
+	// Wait for everything to have definitely finished loading (like maps)
+	await page.waitForLoadState('networkidle');
+
+	// Save screenshot
+	await page.screenshot({ path: `tests/page-previews/${imageName}.png`, fullPage: true });
 }
