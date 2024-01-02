f9924230fd6b96b60f232b6b1953f5c8d32700c5
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-01-02 16:05:32 +0000

    Make a11y test failures human readable

--- tests/a11y.spec.ts
+++ tests/a11y.spec.ts
@@ -19,14 +19,13 @@ test.describe('A11y tests', () => {
 		['desktop', 'Desktop Chrome']
 	]);
 	for (const [viewportName, viewportDevice] of viewPorts) {
-
 		//Home page
 		test(`Home - ${viewportName}`, async ({ page }) => {
 			await page.setViewportSize(devices[viewportDevice].viewport);
 
 			await page.goto('/');
 
-			expect((await accessibilityScanner.analyze()).violations).toEqual([]);
+			await reportAccessibilityViolations(accessibilityScanner);
 		});
 
 		// Content pages
@@ -37,8 +36,42 @@ test.describe('A11y tests', () => {
 
 				await page.goto(route);
 
-				expect((await accessibilityScanner.analyze()).violations).toEqual([]);
+				await reportAccessibilityViolations(accessibilityScanner);
 			});
 		}
 	}
 });
+
+async function reportAccessibilityViolations(accessibilityScanner: AxeBuilder) {
+	// Scan the page with axe to look for a11y violations
+	let violations = (await accessibilityScanner.analyze()).violations;
+
+	// Make Playwright output the key information as a (more) human readable string
+	let violationDescription = '';
+	if (violations.length > 0) {
+		violationDescription = `Warning - ${violations.length} a11y rule violations found\n\n`;
+
+		for (let i = 0; i < violations.length; i++) {
+			let violation = violations[i];
+			violationDescription += `########################################################\n`;
+			violationDescription += `Rule Violation ${i + 1}\n`;
+			violationDescription += `${violation.impact?.toUpperCase()} - ${violation.help}\n`;
+			violationDescription += `${violation.helpUrl}\n`;
+
+			for (let j = 0; j < violation.nodes.length; j++) {
+				let instance = violation.nodes[j];
+
+				violationDescription += `--------------------------------------------------------\n`;
+				violationDescription += `Instance ${j + 1}/${violation.nodes.length}\n\n`;
+
+				violationDescription += `element html: ${instance.html}\n`;
+				violationDescription += `element selector: ${instance.target}\n\n`;
+
+				violationDescription += `${instance.failureSummary}\n\n`;
+			}
+		}
+	}
+
+	// If there are violations then output a human readable failure
+	expect(violations, violationDescription).toHaveLength(0);
+}
