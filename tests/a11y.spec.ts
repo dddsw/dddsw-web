b492c55c82243c05f4924200a247649c1d001207
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-01-03 16:16:33 +0000

    Refactor a11y tests

--- tests/a11y.spec.ts
+++ tests/a11y.spec.ts
@@ -1,48 +1,46 @@
 import { test, devices, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
-test.describe('A11y tests', () => {
-	let accessibilityScanner: AxeBuilder;
+let accessibilityScanner: AxeBuilder;
 
-	test.beforeEach(({ page }) => {
-		accessibilityScanner = new AxeBuilder({ page }).withTags([
-			'wcag2a',
-			'wcag2aa',
-			'wcag21a',
-			'wcag21aa'
-		]);
-	});
-
-	const viewPorts = new Map([
-		['mobile', 'iPhone SE'],
-		['tablet', 'iPad Mini'],
-		['desktop', 'Desktop Chrome']
+test.beforeEach(({ page }) => {
+	accessibilityScanner = new AxeBuilder({ page }).withTags([
+		'wcag2a',
+		'wcag2aa',
+		'wcag21a',
+		'wcag21aa'
 	]);
-	for (const [viewportName, viewportDevice] of viewPorts) {
-		//Home page
-		test(`Home - ${viewportName}`, async ({ page }) => {
-			await page.setViewportSize(devices[viewportDevice].viewport);
-
-			await page.goto('/');
-
-			await reportAccessibilityViolations(accessibilityScanner);
-		});
-
-		// Content pages
-		const routes = ['about', 'sponsorship', '2024', 'code-of-conduct'];
-		for (const route of routes) {
-			test(`${route} - ${viewportName}`, async ({ page }) => {
-				await page.setViewportSize(devices[viewportDevice].viewport);
-
-				await page.goto(route);
-
-				await reportAccessibilityViolations(accessibilityScanner);
-			});
-		}
-	}
 });
 
-async function reportAccessibilityViolations(accessibilityScanner: AxeBuilder) {
+const viewPorts = new Map([
+	['mobile', 'iPhone SE'],
+	['tablet', 'iPad Mini'],
+	['desktop', 'Desktop Chrome']
+]);
+for (const [viewportName, viewportDevice] of viewPorts) {
+	//Home page
+	test(`Home page a11y checks - ${viewportName}`, async ({ page }) => {
+		await page.setViewportSize(devices[viewportDevice].viewport);
+
+		await page.goto('/');
+
+		await reportAccessibilityViolations();
+	});
+
+	// Content pages
+	const routes = ['about', 'sponsorship', '2024', 'code-of-conduct'];
+	for (const route of routes) {
+		test(`${route} page a11y checks - ${viewportName}`, async ({ page }) => {
+			await page.setViewportSize(devices[viewportDevice].viewport);
+
+			await page.goto(route);
+
+			await reportAccessibilityViolations();
+		});
+	}
+}
+
+async function reportAccessibilityViolations() {
 	// Scan the page with axe to look for a11y violations
 	let violations = (await accessibilityScanner.analyze()).violations;
 
