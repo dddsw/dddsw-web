302b067c19fbf72b193385e8241de59254978dcd
Russell Day <russell.day.coder@gmail.com>
2024-02-26 22:55:31 +0000

    Undo - Add basic details on workshop page

--- src/routes/new-speakers-workshop/+page.svelte
+++ src/routes/new-speakers-workshop/+page.svelte
@@ -5,23 +5,40 @@
 
 <div class="secondary-bg">
 	<div class="section">
+		<h1>About</h1>
+		<p>
+			As an inclusive, and community-driven conference we want to encourage everyone to share their experiences and knowledge.<br/>
+			If you are a new or inexperienced speakers submit your talk and register interest in attending here: <a href="https://sessionize.com/ddd-south-west-2024" target="_blank">Call for Speakers</a><br/>
+			<br/>
+			Community stalwart <a href="https://dylanbeattie.net/" target="_blank">Dylan Beattie</a> will be presenting
+			<a href="https://ursatile.com/workshops/from-keyboard-to-keynote.html" target="_blank">Public Speaking: From Keyboard to Keynote</a> in a 1-day format.
+		</p>
+		<h2>Time and Date</h2>
+		<p>Saturday 23rd March - 9am - 4pm</p>
+		<h2>Requirements</h2>
+		<p>
+			<ul>
+				<li>A laptop</li>
+				<li>Presentation software e.g. Microsoft Powerpoint, Google Slides</li>
+			</ul>
+	</div>
+</div>
+<div class="tertiary-bg">
+	<div class="section">
 		<h1>Venue</h1>
 		<p>
-			DDD South West 2024 New Speakers Workshop will be held at <a href="https://www.watershed.co.uk/" target="_blank"
-				>Watershed</a
-			>
-			located at the following address: <br />
+			DDD South West 2024 New Speakers Workshop will be held at <a href="https://www.watershed.co.uk/" target="_blank">Watershed</a> located at the following address: <br />
             1 Canons Road<br />
             Harbourside<br />
             Bristol<br />
             BS1 5TX
 		</p>
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2486.368127790757!2d-2.6008288223885514!3d51.4513972147917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48718dd658d5706f%3A0x78607ce02ba4510!2sWatershed!5e0!3m2!1sen!2suk!4v1708108164734!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title="Map of Watershed venue location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2486.368127790757!2d-2.6008288223885514!3d51.4513972147917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48718dd658d5706f%3A0x78607ce02ba4510!2sWatershed!5e0!3m2!1sen!2suk!4v1708108164734!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 	</div>
 </div>
 
-<div class="tertiary-bg">
+<div class="secondary-bg">
 	<div class="section">
 		<h2>Travel</h2>
         <h3>By bus</h3>
@@ -66,7 +83,7 @@
 	</div>
 </div>
 
-<div class="secondary-bg">
+<div class="tertiary-bg">
 	<div class="section">
 		<h1>Covid policy</h1>
 		<p>
