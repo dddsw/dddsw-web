33923ca772aa9be82e52661a436105f55cccc4f0
Russell Day <russell.day.coder@gmail.com>
2024-04-10 15:18:55 +0100

    Replace existing code of conduct, add internal policy

--- src/routes/code-of-conduct/+page.svelte
+++ src/routes/code-of-conduct/+page.svelte
@@ -11,46 +11,63 @@
 			the following code of conduct.
 		</p>
 		<p>
-			Our conference is dedicated to providing a harassment-free conference experience for everyone,
-			regardless of gender, age, sexual orientation, disability, physical appearance, body size,
-			race, or religion (or lack thereof). We do not tolerate harassment of conference participants
-			in any form. Sexual language and imagery is not appropriate for any conference venue,
-			including talks, workshops, parties, Twitter and other online media. Conference participants
-			violating these rules may be sanctioned or expelled from the conference at the discretion of
-			the conference organisers.
+			DDD South West is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age or religion. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any conference venue, including talks. Conference participants violating these rules may be sanctioned or expelled from the conference at the discretion of the conference organizers.
 		</p>
 		<p>
-			Harassment includes offensive verbal comments related to gender, age, sexual orientation,
-			disability, physical appearance, body size, race, religion, sexual images in public spaces,
-			deliberate intimidation, stalking, following, harassing photography or recording, sustained
-			disruption of talks or other events, inappropriate physical contact, and unwelcome sexual
-			attention. Participants asked to stop any harassing behavior are expected to comply
-			immediately.
+			Harassment includes, but is not limited to:
 		</p>
+		<ul>
+			<li>Verbal comments that reinforce social structures of domination related to gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age, or religion.</li>
+			<li>Sexual images in public spaces</li>
+			<li>Deliberate intimidation, stalking, or following </li>
+			<li>Harassing photography or recording</li>
+			<li>Sustained disruption of talks or other events</li>
+			<li>Inappropriate physical contact</li>
+			<li>Unwelcome sexual attention</li>
+			<li>Advocating for, or encouraging, any of the above behaviour</li>
+		</ul>
 		<p>
 			Sponsors are also subject to the anti-harassment policy. In particular, sponsors should not
 			use sexualised images, activities, or other material. Booth staff (including volunteers)
 			should not use sexualised clothing/uniforms/costumes, or otherwise create a sexualised
 			environment.
 		</p>
+		<h2>Enforcement</h2>
 		<p>
-			If a participant engages in harassing behavior, the conference organisers may take any action
-			they deem appropriate, including warning the offender or expulsion from the conference with no
-			refund.
+			Participants asked to stop any harassing behavior are expected to comply immediately
+
+			Exhibitors in the expo hall, sponsor or vendor booths, or similar activities are also subject to the anti-harassment policy. In particular, exhibitors should not use sexualized images, activities, or other material. Booth staff (including volunteers) should not use sexualized clothing/uniforms/costumes, or otherwise create a sexualized environment.
+		
+			If a participant engages in harassing behaviour, event organisers retain the right to take any actions to keep the event a welcoming environment for all participants. This includes warning the offender or expulsion from the conference.
+		
+			Event organisers may take action to redress anything designed to, or with the clear impact of, disrupting the event or making the environment hostile for any participants.
+		
+			We expect participants to follow these rules at all event venues and event-related social activities. We think people should follow these rules outside event activities too!
 		</p>
+		<h2>Reporting</h2>
 		<p>
-			If you are being harassed, notice that someone else is being harassed, or have any other
-			concerns, please contact a member of conference staff immediately. Conference staff can be
-			identified as they’ll be wearing the conference t-shirts.
+			If someone makes you or anyone else feel unsafe or unwelcome, please report it as soon as possible. Conference staff can be identified by t-shirts/special badges/head sets. Harassment and other code of conduct violations reduce the value of our event for everyone. We want you to be happy at our event. People like you make our event a better place.
+
+			You can make a report either personally or anonymously.
 		</p>
+		<h2>Anonymous Report</h2>
 		<p>
-			Conference staff will be happy to help participants contact hotel/venue security or local
-			police, provide escorts, or otherwise assist those experiencing harassment to feel safe for
-			the duration of the conference. We value your attendance.
+			You can make an anonymous report <a href="https://forms.office.com/e/ZDSiDR6Tyg" target="_blank">here</a>.
+
+			We can't follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.
 		</p>
-		<p class="emphasis">
-			We expect participants to follow these rules at conference and workshop venues and
-			conference-related social events.
+		<h2>Personal Report</h2>
+		<p>
+			You can make a personal report by:
+		</p>
+		<ul>
+			<li>Calling or messaging this phone number: <a href="tel:+447984465333">+447984465333</a>. This phone number will be continuously monitored for the duration of the event.</li>
+			<li>Contacting a staff member, identified by DDD South West 2024 t-shirts or lanyards.</li>
+		</ul>
+		<p>
+			When taking a personal report, our staff will ensure you are safe and cannot be overheard. They may involve other event staff to ensure your report is managed properly. Once safe, we'll ask you to tell us about what happened. This can be upsetting, but we'll handle it as respectfully as possible, and you can bring someone to support you. You won't be asked to confront anyone and we won't tell anyone who you are.
+		
+			Our team will be happy to help you contact hotel/venue security, local law enforcement, local support services, provide escorts, or otherwise assist you to feel safe for the duration of the event. We value your attendance.
 		</p>
 	</div>
 </div>
