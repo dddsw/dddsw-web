141372489bbb22ebde0999219ee1fad5f8a3fd7f
Russell Day <russell.day.coder@gmail.com>
2024-04-18 12:10:09 +0100

    Added p tags around most statements to make it more readable

--- src/routes/code-of-conduct/+page.svelte
+++ src/routes/code-of-conduct/+page.svelte
@@ -10,12 +10,13 @@
 			All attendees, speakers, sponsors and volunteers at our conference are required to agree with
 			the following code of conduct.
 		</p>
-		<p>
-			DDD South West is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age or religion. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any conference venue, including talks. Conference participants violating these rules may be sanctioned or expelled from the conference at the discretion of the conference organizers.
-		</p>
-		<p>
-			Harassment includes, but is not limited to:
-		</p>
+		<p>DDD South West is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age or religion.</p>
+
+		<p>We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any conference venue, including talks.</p>
+
+		<p>Conference participants violating these rules may be sanctioned or expelled from the conference at the discretion of the conference organizers.</p>
+
+		<p>Harassment includes, but is not limited to:</p>
 		<ul>
 			<li>Verbal comments that reinforce social structures of domination related to gender, gender identity and expression, sexual orientation, disability, physical appearance, body size, race, age, or religion.</li>
 			<li>Sexual images in public spaces</li>
@@ -26,33 +27,36 @@
 			<li>Unwelcome sexual attention</li>
 			<li>Advocating for, or encouraging, any of the above behaviour</li>
 		</ul>
-		<p>
-			In this document the term 'participant' refers to any person at the event including, but not limited to; ticketed attendees, sponsor staff, volunteers.
 
-			Reporting, enforcement, and feedback policies apply to all participants.
-		</p>
+		<p>In this document the term 'participant' refers to any person at the event including, but not limited to; ticketed attendees, sponsor staff, volunteers.</p>
+
+		<p>Reporting, enforcement, and feedback policies apply to all participants.</p>
+
 		<h2>Enforcement</h2>
-		<p>
-			Participants asked to stop any harassing behaviour are expected to comply immediately.
+		<p>Participants asked to stop any harassing behaviour are expected to comply immediately.</p>
 		
-			If a participant engages in harassing behaviour, event organisers retain the right to take any actions to keep the event a welcoming environment for all participants. This includes warning the offender or expulsion from the conference.
+		<p>If a participant engages in harassing behaviour, event organisers retain the right to take any actions to keep the event a welcoming environment for all participants. This includes warning the offender or expulsion from the conference.</p>
 		
-			Event organisers may take action to redress anything designed to, or with the clear impact of, disrupting the event or making the environment hostile for any participants.
+		<p>Event organisers may take action to redress anything designed to, or with the clear impact of, disrupting the event or making the environment hostile for any participants.</p>
 		
-			We expect participants to follow these rules at all event venues and event-related social activities. We think people should follow these rules outside event activities too!
-		</p>
+		<p>We expect participants to follow these rules at all event venues and event-related social activities. We think people should follow these rules outside event activities too!</p>
+
 		<h2>Reporting</h2>
-		<p>
-			If someone makes you or anyone else feel unsafe or unwelcome, please report it as soon as possible. Conference staff can be identified by t-shirts/special badges/head sets. Harassment and other code of conduct violations reduce the value of our event for everyone. We want you to be happy at our event. People like you make our event a better place.
+		<p>If someone makes you or anyone else feel unsafe or unwelcome, please report it as soon as possible.</p>
 
-			You can make a report either personally or anonymously.
-		</p>
+		<p>Conference staff can be identified by t-shirts/special badges/head sets.</p>
+
+		<p>Harassment and other code of conduct violations reduce the value of our event for everyone.</p>
+
+		<p>We want you to be happy at our event. People like you make our event a better place.</p>
+
+		<p>You can make a report either personally or anonymously.</p>
+
 		<h2>Anonymous Report</h2>
-		<p>
-			You can make an anonymous report <a href="https://forms.office.com/e/ZDSiDR6Tyg" target="_blank">here</a>.
+		<p>You can make an anonymous report <a href="https://forms.office.com/e/ZDSiDR6Tyg" target="_blank">here</a>.</p>
 
-			We can't follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.
-		</p>
+		<p>We can't follow up an anonymous report with you directly, but we will fully investigate it and take whatever action is necessary to prevent a recurrence.</p>
+
 		<h2>Personal Report</h2>
 		<p>
 			You can make a personal report by:
@@ -61,11 +65,21 @@
 			<li>Calling or messaging this phone number: <a href="tel:+447984465333">+447984465333</a>. This phone number will be continuously monitored for the duration of the event.</li>
 			<li>Contacting a staff member, identified by DDD South West 2024 t-shirts or lanyards.</li>
 		</ul>
-		<p>
-			When taking a personal report, our staff will ensure you are safe and cannot be overheard. They may involve other event staff to ensure your report is managed properly. Once safe, we'll ask you to tell us about what happened. This can be upsetting, but we'll handle it as respectfully as possible, and you can bring someone to support you. You won't be asked to confront anyone and we won't tell anyone who you are.
-		
-			Our team will be happy to help you contact hotel/venue security, local law enforcement, local support services, provide escorts, or otherwise assist you to feel safe for the duration of the event. We value your attendance.
-		</p>
+
+		<p>When taking a personal report, our staff will ensure you are safe and cannot be overheard.</p>
+
+		<p>They may involve other event staff to ensure your report is managed properly.</p>
+
+		<p>Once safe, we'll ask you to tell us about what happened.</p>
+
+		<p>This can be upsetting, but we'll handle it as respectfully as possible, and you can bring someone to support you.</p>
+
+		<p>You won't be asked to confront anyone and we won't tell anyone who you are.</p>
+
+		<p>Our team will be happy to help you contact hotel/venue security, local law enforcement, local support services, provide escorts, or otherwise assist you to feel safe for the duration of the event.</p>
+
+		<p>We value your attendance.</p>
+
 		<h3>Credit</h3>
         <p>
             This anti-harassment policy is based on the example policy from the Geek Feminism wiki, created by the Ada Initiative and other volunteers.
