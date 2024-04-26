462640743d1d2a4de0a5b4bcac214fa3cb7490c2
Sara Gowen <me@dynamictulip.com>
2024-04-26 23:53:45 +0100

    Add room map of Engine shed to website

---
+++ src/routes/venue-layout/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { pageTitle } from '../../stores.js';
+	pageTitle.set('Engine Shed room layout');
+</script>
+
+<div class="secondary-bg">
+	<img
+		class="map"
+		src="../images/venue-layout/engine-shed-layout.png"
+		alt="map of the room layout"
+	/>
+</div>
+
+<style>
+	.map {
+		width: 100%;
+		height: auto;
+	}
+</style>
