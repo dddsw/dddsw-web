55f405c0a8c633f189ffff1953cc5414a670f4c9
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-10-13 11:44:59 +0100

    Fix linting on svelte files

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import svelteParser from 'svelte-eslint-parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,11 +14,34 @@ const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default [
 	includeIgnoreFile(gitignorePath),
-	{ files: ['**/*.{js,mjs,cjs,ts,svelte}'] },
-	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
-	...eslintPluginSvelte.configs['flat/recommended'],
 	...eslintPluginSvelte.configs['flat/prettier'],
-	eslintConfigPrettier
+	eslintConfigPrettier,
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte', '*.svelte'],
+
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			},
+			parser: svelteParser,
+			parserOptions: {
+				parser: {
+					ts: '@typescript-eslint/parser'
+				},
+				extraFileExtensions: ['.svelte']
+			}
+		}
+	}
 ];
