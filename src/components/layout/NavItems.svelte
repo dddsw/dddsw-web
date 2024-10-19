3e24e4db51afab142c5eb66a5181e733786bb2f2
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-10-19 17:31:32 +0100

    Add navbar

---
+++ src/components/layout/NavItems.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	type NavItem = {
+		hidden: boolean;
+		href: string;
+		text: string;
+	};
+
+	const navItems: NavItem[] = [
+		{ text: 'Home', href: '/', hidden: false },
+		{ text: 'About', href: '/about', hidden: false },
+		{ text: 'Code of Conduct', href: '/code-of-conduct', hidden: false }
+	];
+</script>
+
+{#each navItems as navItem}
+	{#if !navItem.hidden}
+		<li>
+			<a class:active={navItem.href === $page.url.pathname} href={navItem.href}>{navItem.text}</a>
+		</li>
+	{/if}
+{/each}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,37 @@
 <script lang="ts">
-	import Header from '$lib/Header.svelte';
-	import Footer from '$lib/Footer.svelte';
-
 	import { page } from '$app/stores';
 	import '../app.css';
+	import Navbar from '../components/layout/Navbar.svelte';
 </script>
 
 <svelte:head>
 	<title>{$page.data.title}</title>
 </svelte:head>
 
-<Header />
+<header>
+	<a href="/">
+		<img src="../images/logo.png" alt="The DDD South West logo" />
+	</a>
+	<Navbar />
+	<h1 class="text-3xl font-bold underline">{$page.data.title}</h1>
+</header>
 
 <main class="prose">
 	<slot />
 </main>
 
-<Footer />
+<footer>
+	<div>
+		<div>
+			<h2>By the community, for the community</h2>
+		</div>
+	</div>
+	<div>
+		<a href="https://twitter.com/dddsouthwest" target="_blank">
+			<i class="fa-brands fa-twitter fa-3x" title="DDD South West Twitter" />
+		</a>
+		<a href="https://www.linkedin.com/company/ddd-south-west/" target="_blank">
+			<i class="fa-brands fa-linkedin fa-3x" title="DDD South West LinkedIn" />
+		</a>
+	</div>
+</footer>
