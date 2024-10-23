dfbbf492cc1e2a22fba33809abcaccdb0e6528ae
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-10-23 12:44:22 +0100

    Switch to bumblebee theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,22 +10,7 @@ const config: TailwindConfig = {
 	plugins: [typography, daisyui],
 
 	daisyui: {
-		themes: [
-			{
-				mytheme: {
-					primary: '#ff9930',
-					secondary: 'white',
-					accent: '#eaeaea',
-					neutral: '#96c6ca',
-					'base-100': '#ffffff',
-					info: '#0000ff',
-					success: '#00ff00',
-					warning: '#00ff00',
-					error: '#ff0000'
-				}
-			},
-			'bumblebee'
-		],
+		themes: ['bumblebee'],
 		darkTheme: 'dark', // name of one of the included themes for dark mode
 		base: true, // applies background color and foreground color for root element by default
 		styled: true, // include daisyUI colors and design decisions for all components
