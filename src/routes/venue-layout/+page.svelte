cd24caa322ef7ad74ee110391860d47ce81539af
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-04-02 18:36:34 +0100

    Use new svg map

--- src/routes/venue-layout/+page.svelte
+++ src/routes/venue-layout/+page.svelte
@@ -1,19 +1,41 @@
+<svelte:options runes={true} />
+
 <script lang="ts">
+	import EngineShed from './EngineShed.svelte';
 	import { pageTitle } from '../../stores.js';
+	import * as Panzoom from '@panzoom/panzoom';
+	import { onMount } from 'svelte';
+
 	pageTitle.set('Engine Shed room layout');
+
+	onMount(() => {
+		const svg = document.getElementById('svg1')!;
+		const view = svg.parentElement!;
+
+		const panzoom = Panzoom.default(svg);
+
+		view.addEventListener('wheel', panzoom.zoomWithWheel);
+	});
 </script>
 
-<div class="secondary-bg">
-	<img
-		class="map"
-		src="../images/venue-layout/engine-shed-layout.png"
-		alt="map of the room layout"
-	/>
+<div class="section">
+	<div class="map">
+		<EngineShed />
+	</div>
 </div>
 
 <style>
 	.map {
 		width: 100%;
-		height: auto;
+		height: 70vh;
+		border-color: black;
+		border-width: 1px;
+		border-style: solid;
+		overflow: hidden;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 20px 0px;
+		border-radius: 25px;
 	}
 </style>
