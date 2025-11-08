563a9af8bb3d12eba5eafe44413609f776b533f0
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-11-08 14:00:02 +0000

    Move Timeline to component

---
+++ src/lib/Timeline.svelte
@@ -0,0 +1,74 @@
+<h2>Timeline</h2>
+<div class="timeline text-center">
+	<span class="timeline-item">
+		<span class="material-symbols-outlined"> circle </span>
+		<p>31st January</p>
+		<p>Call for speakers opens</p>
+	</span>
+	<span class="timeline-item">
+		<span class="material-symbols-outlined"> circle </span>
+		<p>7th March</p>
+		<p>Call for speakers closes</p>
+	</span>
+	<span class="timeline-item">
+		<span class="material-symbols-outlined"> circle </span>
+		<p>14th March</p>
+		<p>Session voting opens</p>
+	</span>
+	<span class="timeline-item">
+		<span class="material-symbols-outlined"> circle </span>
+		<p>28th March</p>
+		<p>Session voting closes</p>
+	</span>
+	<span class="timeline-item">
+		<span class="material-symbols-outlined"> circle </span>
+		<p>18th April</p>
+		<p>Schedule announced and <br />ticket registration opens</p>
+	</span>
+	<span class="timeline-item">
+		<span class="material-symbols-outlined"> circle </span>
+		<p>16th May</p>
+		<p>Day of the event!</p>
+	</span>
+</div>
+<sub class="sub-highlight"
+	>We'll try our best to keep to these dates but please be patient - DDDSW is entirely volunteer
+	driven ❤️
+</sub>
+
+<style>
+	.timeline {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+	}
+
+	.timeline p {
+		font-size: 0.75rem;
+		margin: 0;
+		font-weight: bold;
+	}
+
+	.timeline-item p:first-of-type {
+		border-bottom: #cdcdcd 2px dotted;
+		margin-bottom: 5px;
+		font-weight: normal;
+	}
+
+	.timeline-item {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		flex: 1 1 0;
+	}
+
+	.sub-highlight {
+		font-style: italic;
+	}
+
+	@media (min-width: 576px) {
+		.timeline {
+			flex-direction: row;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import Sponsors from '$lib/Sponsors.svelte';
 	import type { KeyPoint } from '../types/KeyPoint.type';
 	import { pageTitle } from '../stores.js';
+	import Timeline from '$lib/Timeline.svelte';
 
 	pageTitle.set('DDD South West');
 
@@ -56,78 +57,6 @@
 
 <div class="secondary-bg">
 	<div class="section">
-		<h2>Timeline</h2>
-		<div class="timeline text-center">
-			<span class="timeline-item">
-				<span class="material-symbols-outlined"> circle </span>
-				<p>31st January</p>
-				<p>Call for speakers opens</p>
-			</span>
-			<span class="timeline-item">
-				<span class="material-symbols-outlined"> circle </span>
-				<p>7th March</p>
-				<p>Call for speakers closes</p>
-			</span>
-			<span class="timeline-item">
-				<span class="material-symbols-outlined"> circle </span>
-				<p>14th March</p>
-				<p>Session voting opens</p>
-			</span>
-			<span class="timeline-item">
-				<span class="material-symbols-outlined"> circle </span>
-				<p>28th March</p>
-				<p>Session voting closes</p>
-			</span>
-			<span class="timeline-item">
-				<span class="material-symbols-outlined"> circle </span>
-				<p>18th April</p>
-				<p>Schedule announced and <br />ticket registration opens</p>
-			</span>
-			<span class="timeline-item">
-				<span class="material-symbols-outlined"> circle </span>
-				<p>16th May</p>
-				<p>Day of the event!</p>
-			</span>
-		</div>
-		<sub class="sub-highlight"
-			>We'll try our best to keep to these dates but please be patient - DDDSW is entirely volunteer
-			driven ❤️</sub>
+		<Timeline />
 	</div>
 </div>
-
-<style>
-	.timeline {
-		display: flex;
-		flex-direction: column;
-		gap: 20px;
-	}
-
-	.timeline p {
-		font-size: 0.75rem;
-		margin: 0;
-		font-weight: bold;
-	}
-
-	.timeline-item p:first-of-type {
-		border-bottom: #cdcdcd 2px dotted;
-		margin-bottom: 5px;
-		font-weight: normal;
-	}
-
-	.timeline-item {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		flex: 1 1 0;
-	}
-
-	.sub-highlight {
-		font-style: italic;
-	}
-
-	@media (min-width: 576px) {
-		.timeline {
-			flex-direction: row;
-		}
-	}
-</style>
