72e8dd953c05eb0657a0219cc188de3ff2311c2c
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-11-08 16:06:47 +0000

    Trigger updates on milestones

--- src/lib/LatestUpdate.svelte
+++ src/lib/LatestUpdate.svelte
@@ -1,70 +1,44 @@
 <script lang="ts">
-	type Update = {
-		dateFrom: Date;
-		content: string;
-	};
+	import { Milestone, get } from '$lib/milestones';
 
 	let linkedInIcon = `<a href="https://www.linkedin.com/company/ddd-south-west/" target="_blank">
 							<i class="fa-brands fa-linkedin fa-lg" title="DDD South West LinkedIn" aria-hidden="true"></i>
 							<span class="fa-sr-only">DDD South West LinkedIn</span>
 						</a>`;
 
-	let allUpdates: Update[] = [
-		{
-			dateFrom: new Date('2025-10-29'),
-			content: `DDD South West 2026 planning has begun! More info coming soon 😎`
-		},
-		{
-			dateFrom: new Date('2024-12-20'),
-			content: `DDD South West is coming... save the date! See you on the
-			<strong class="emphasis">26th April 2025</strong> at Engine Shed, Bristol`
-		},
-		{
-			dateFrom: new Date('2024-12-25'),
-			content: `Watch this space.... call for speakers is opening on 1st January 2025.`
-		},
-		{
-			dateFrom: new Date('2025-01-01'),
-			content: `Call for speakers is <strong class="emphasis">open until 31st January 2025</strong>. 
-            We are a friendly developers conference and welcome papers from speakers both old and new. 
-            So whether you have an established talk you'd like to bring to Bristol or an idea you'd like help developing,
-            <a href="https://sessionize.com/ddd-south-west-2025/">submit your talk now</a>.`
-		},
-		{
-			dateFrom: new Date('2025-02-01'),
-			content: `Call for speakers is now closed. Session voting will be opening soon, watch this space or follow us on ${linkedInIcon}`
-		},
-		{
-			dateFrom: new Date('2025-02-12'),
-			content: `Session voting is now open! Take a look and <a href="https://buff.ly/4hzncRk">vote for your favourites here</a>.<br>Voting will close on 21st February.`
-		},
-		{
-			dateFrom: new Date('2025-02-22'),
-			content: `Session voting has closed, hold tight - ticket registration will be opening at the end of March`
-		},
-		{
-			dateFrom: new Date('2025-03-31'),
-			content: `Speakers have been announced and the schedule is live! <a href="/schedule">Check full schedule details here</a>. Get your tickets from <a href="https://app.tickettailor.com/events/dddsouthwest/1643112/r/website">Ticket Tailor</a>.`
-		},
-		{
-			dateFrom: new Date('2025-04-22'),
-			content: `The day is fast approaching, we're fully sold out but you can still join the waitlist on <a href="https://app.tickettailor.com/events/dddsouthwest/1643112/r/website">Ticket Tailor</a>.`
-		},
-		{
-			dateFrom: new Date('2025-04-26'),
-			content: `Today is the day! See you at Engine Shed and be sure to use <a href="https://pocket2025.dddsouthwest.com">Pocket DDD</a> to give feedback to our wonderful speakers`
-		},
-		{
-			dateFrom: new Date('2025-04-27'),
-			content: `Thank you to everyone for another great DDD South West. See you in 2026 😉`
-		}
-	];
+	const sessionizeUrl = 'https://sessionize.com/ddd-south-west-2026/';
+	const votingUrl = 'https://buff.ly/4hzncRk';
+	const ticketTailorUrl = '...';
+	const pocketdddUrl = 'https://pocket2026.dddsouthwest.com';
 
-	// The current update is the most recent one - take the latest one whose "dateFrom" is in the past
-	let currentUpdate = allUpdates
-		.filter((u) => u.dateFrom.valueOf() < Date.now())
-		.sort((a, b) => a.dateFrom.valueOf() - b.dateFrom.valueOf())
-		.pop();
+	let currentUpdate: string | undefined;
+
+	if (get(Milestone.StartPlanning)?.hasHappened)
+		currentUpdate = `DDD South West 2026 planning has begun! More info coming soon 😎`;
+	else if (get(Milestone.SetADate)?.hasHappened)
+		currentUpdate = `DDD South West is coming... save the date! See you on the
+			<strong class="emphasis">${get(Milestone.TheActualEventDay)?.formattedDate}</strong>`;
+	else if (get(Milestone.OpenCallForSpeakers))
+		currentUpdate = `Watch this space.... call for speakers is opening on ${get(Milestone.OpenCallForSpeakers)!.formattedDate}.`;
+	else if (get(Milestone.OpenCallForSpeakers)?.hasHappened)
+		currentUpdate = `Call for speakers is <strong class="emphasis">open until ${get(Milestone.CloseCallForSpeakers)!.formattedDate}</strong>.
+		    We are a friendly developers conference and welcome papers from speakers both old and new.
+		    So whether you have an established talk you'd like to bring to Bristol or an idea you'd like help developing,
+		    <a href="${sessionizeUrl}">submit your talk now</a>.`;
+	else if (get(Milestone.CloseCallForSpeakers)?.hasHappened)
+		currentUpdate = `Call for speakers is now closed. Session voting will be opening soon, watch this space or follow us on ${linkedInIcon}`;
+	else if (get(Milestone.OpenSessionVoting)?.hasHappened)
+		currentUpdate = `Session voting is now open! Take a look and <a href="${votingUrl}">vote for your favourites here</a>.<br>Voting will close on ${get(Milestone.CloseCallForSpeakers)!.formattedDate}.`;
+	else if (get(Milestone.CloseSessionVoting)?.hasHappened)
+		currentUpdate = `Session voting has closed, hold tight - ticket registration will be opening on ${get(Milestone.OpenTicketRegistration)!.formattedDate}`;
+	else if (get(Milestone.AnnounceScheduleAndOpenTicketRegistration)?.hasHappened)
+		currentUpdate = `Speakers have been announced and the schedule is live! <a href="/schedule">Check full schedule details here</a>. Get your tickets from <a href="${ticketTailorUrl}">Ticket Tailor</a>.`;
+	else if (get(Milestone.SoldOut)?.hasHappened)
+		currentUpdate = `The day is fast approaching, we're fully sold out but you can still join the waitlist on <a href="${ticketTailorUrl}">Ticket Tailor</a>.`;
+	else if (get(Milestone.TheActualEventDay)?.hasHappened)
+		currentUpdate = `Today is the day! See you at Engine Shed and be sure to use <a href="${pocketdddUrl}">Pocket DDD</a> to give feedback to our wonderful speakers`;
+	else if (get(Milestone.ConkedOut)?.hasHappened)
+		currentUpdate = `Thank you to everyone for another great DDD South West. See you in ${get(Milestone.ConkedOut)!.targetDate!.getFullYear() + 1} 😉`;
 </script>
 
 {#if currentUpdate}
@@ -72,7 +46,7 @@
 		<div class="section">
 			<h2>Latest update</h2>
 			<p>
-				{@html currentUpdate.content}
+				{@html currentUpdate}
 			</p>
 		</div>
 	</div>
