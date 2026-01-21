4530b55d4fd9ce3885dfbadeb2606d1fd688c4c5
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2026-01-21 22:27:11 +0000

    Move event details to a common place

---
+++ src/lib/eventDetails.ts
@@ -0,0 +1,10 @@
+export const eventYear = 2026;
+export const eventYearTwoDigit = eventYear - 2000;
+
+const sessionizeApiId = '5mpx9tl7';
+export const sessionizeApiUrl = `https://sessionize.com/api/v2/${sessionizeApiId}`;
+export const sessionizePublicUiUrl = `https://sessionize.com/ddd-south-west-${eventYear}/`;
+
+export const votingUrl = 'https://buff.ly/4hzncRk';
+export const ticketTailorUrl = '...';
+export const pocketdddUrl = `https://pocket${eventYear}.dddsouthwest.com`;
