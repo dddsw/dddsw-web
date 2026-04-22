c1909e07d7f532afb6b73bc5acf40d561aba2083
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2026-04-22 22:41:24 +0100

    Refactor sessionize API URLs to use constants from eventDetails

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,9 @@
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
+import { sessionizeEmbeddedCodeApiUrl } from '$lib/eventDetails.js';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const scheduleRsp = await fetch('https://sessionize.com/api/v2/rp6vdsbs/view/SpeakerWall');
+	const scheduleRsp = await fetch(`${sessionizeEmbeddedCodeApiUrl}/view/SpeakerWall`);
 	if (!scheduleRsp.ok) {
 		throw error(scheduleRsp.status, 'Failed to load speaker wall');
 	}
--- src/routes/sessions/+page.server.ts
+++ src/routes/sessions/+page.server.ts
@@ -1,14 +1,14 @@
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
-import { sessionizeApiUrl } from '$lib/eventDetails.js';
+import { sessionizeJsonApiUrl } from '$lib/eventDetails.js';
 import { Milestone, get } from '$lib/milestones';
 import type { Session } from '../../types/Session.type.js';
 import type { Speaker } from '../../types/Speaker.type.js';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	const [sessionsRsp, speakersRsp] = await Promise.all([
-		fetch(`${sessionizeApiUrl}/view/Sessions`),
-		fetch(`${sessionizeApiUrl}/view/Speakers`)
+		fetch(`${sessionizeJsonApiUrl}/view/Sessions`),
+		fetch(`${sessionizeJsonApiUrl}/view/Speakers`)
 	]);
 
 	if (!sessionsRsp.ok) {
